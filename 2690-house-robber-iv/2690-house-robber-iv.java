7e8ce47404ed91938a5fcc432762a088bb444d5e
Nakul Bhar <115773691+Nakkkkkul0130@users.noreply.github.com>
2025-03-15 18:19:52 +0530

    Time: 16 ms (99.5%), Space: 55.9 MB (79.1%) - LeetHub

--- 2690-house-robber-iv/2690-house-robber-iv.java
+++ 2690-house-robber-iv/2690-house-robber-iv.java
@@ -1,30 +1,31 @@
 class Solution {
     public int minCapability(int[] nums, int k) {
-        int left=Integer.MAX_VALUE, right = Integer.MIN_VALUE;
-        for(int num : nums){
-            left = Math.min(left,num);
-            right = Math.max(right,num);
+        int left = Integer.MAX_VALUE, right = Integer.MIN_VALUE;
+
+        for (int num : nums) {
+            left = Math.min(left, num);
+            right = Math.max(right, num);
         }
-        while(left<right){
-            int mid = left+(right-left)/2;
-            if(canrob(nums,k,mid)){
-                right=mid;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            int count = 0, i = 0;
+
+            while (i < nums.length) {
+                if (nums[i] <= mid) {
+                    count++; 
+                    i++; 
+                }
+                i++; 
             }
-            else{
-                left=mid+1;
+
+            if (count >= k) {
+                right = mid;
+            } else {
+                left = mid + 1;
             }
         }
-        return left;
-    }
-    private boolean canrob(int[] nums, int k, int capable){
-        int cnt=0; int i=0;
-        while(i<nums.length){
-            if(nums[i]<=capable){
-                cnt++;
-                i++;
-            }
-            i++;
-        }
-        return cnt>=k;
+
+        return left; 
     }
 }
