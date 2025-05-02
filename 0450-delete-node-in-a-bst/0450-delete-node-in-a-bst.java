a00a0c3a0d8265d9b1f9bf669216be742b921c23
Nakul Bhar <115773691+Nakkkkkul0130@users.noreply.github.com>
2025-05-02 18:57:31 +0530

    Time: 0 ms (100%), Space: 45.9 MB (78.05%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.java
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.java
@@ -19,14 +19,12 @@ class Solution {
         else {
             if(key < root.val) root.left = deleteNode(root.left, key);
             else if(key > root.val) root.right = deleteNode(root.right, key);
-            else {
-               
+            else {   
                 if(root.left == null) return root.right;
                 else if(root.right == null) return root.left;
                 else {
                     root.val = minValue(root.right);
-                    root.right = deleteNode(root.right, root.val);
-                    
+                    root.right = deleteNode(root.right, root.val);                 
                 }
             }
         }
