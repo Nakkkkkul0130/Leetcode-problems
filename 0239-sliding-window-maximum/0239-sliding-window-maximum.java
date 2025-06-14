38069b8c1b444ba650485af968c7100bcb010a6c
Nakul Bhar <115773691+Nakkkkkul0130@users.noreply.github.com>
2025-06-15 00:41:35 +0530

    Time: 32 ms (52.79%), Space: 58.4 MB (77.14%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.java
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -4,13 +4,20 @@ class Solution {
         int[] res = new int[n-k+1];
         Deque<Integer> dq = new LinkedList<>();
         for(int i=0;i<n;i++){
+            //removing out of bound index from front
+
             while(!dq.isEmpty() && dq.peekFirst()<=i-k){
                 dq.pollFirst();
             }
+            //remove all smaller element from back
+
             while(!dq.isEmpty() && nums[i]>=nums[dq.peekLast()]){
                 dq.pollLast();
             }
+            //adding current index
             dq.offerLast(i);
+
+            //Adding max to result once the first window processed
             if(i>=k-1){
                 res[i-k+1]=nums[dq.peekFirst()];
             }
