6c13a29386332d2bdb476bdd2f9725c70edf86ce
Nakul Bhar <115773691+Nakkkkkul0130@users.noreply.github.com>
2025-06-19 11:04:23 +0530

    Time: 31 ms (89.72%), Space: 56.7 MB (78.01%) - LeetHub

--- 2387-partition-array-such-that-maximum-difference-is-k/2387-partition-array-such-that-maximum-difference-is-k.java
+++ 2387-partition-array-such-that-maximum-difference-is-k/2387-partition-array-such-that-maximum-difference-is-k.java
@@ -1,12 +1,13 @@
 class Solution {
     public int partitionArray(int[] nums, int k) {
         Arrays.sort(nums);
-        int count=1;
+        int count=1; // We need at least one group
         int groupStart = nums[0];
         for(int i=1;i<nums.length;i++){
+            // If current number can't go into this group
             if(nums[i]-groupStart>k){
-                count++;
-                groupStart = nums[i];
+                count++;    // Making a new group
+                groupStart = nums[i];   // Starting new group from here
             }
         }
         return count;
