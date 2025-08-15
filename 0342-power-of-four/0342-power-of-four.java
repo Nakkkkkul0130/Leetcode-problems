a6aac0cf7d8ae8a3441c15a95bf768ccfc2bb9bf
Nakul Bhar <115773691+Nakkkkkul0130@users.noreply.github.com>
2025-08-15 22:16:36 +0530

    Time: 0 ms (100%), Space: 40.8 MB (77.96%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.java
+++ 0342-power-of-four/0342-power-of-four.java
@@ -1,11 +1,15 @@
 class Solution {
     public boolean isPowerOfFour(int n) {
+        if(n<=0){
+            return false;
+        }
         if(n==1){
             return true;
         }
-        if(n%4!=0 || n==0){
-            return false;
+        while(n%4==0){
+            n/=4;
         }
-        return isPowerOfFour(n/4);
+        return n==1;
+
     }
 }
