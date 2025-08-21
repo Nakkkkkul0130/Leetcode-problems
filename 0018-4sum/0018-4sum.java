90a241d1ad1eb058638a2121ccb42fab948d56eb
Nakul Bhar <115773691+Nakkkkkul0130@users.noreply.github.com>
2025-08-22 00:15:56 +0530

    Time: 87 ms (10.68%), Space: 106.7 MB (7.07%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -1,40 +1,39 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
         Arrays.sort(nums);
-        List<List<Integer>> result = new ArrayList<>();
+        List<List<Integer>> res = new ArrayList<>();
         int n = nums.length;
 
-        for (int i = 0; i < n - 3; i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) continue; 
-
-            for (int j = i + 1; j < n - 2; j++) {
-                if (j > i + 1 && nums[j] == nums[j - 1]) continue;
-
-                int l = j + 1;
-                int r = n - 1;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int l = j + 1;       
+                int r = n - 1;       
 
                 while (l < r) {
                     long sum = (long) nums[i] + nums[j] + nums[l] + nums[r];
 
                     if (sum == target) {
-                        result.add(Arrays.asList(nums[i], nums[j], nums[l], nums[r]));
-
-                        while (l < r && nums[l] == nums[l + 1]) l++;
-                        while (l < r && nums[r] == nums[r - 1]) r--;
-
+                        List<Integer> li = new ArrayList<>();
+                        li.add(nums[i]);
+                        li.add(nums[j]);
+                        li.add(nums[l]);
+                        li.add(nums[r]);
+                        res.add(li);
                         l++;
                         r--;
-                    } 
+                    }
                     else if (sum < target) {
-                        l++;
+                        l++;  
                     } 
                     else {
-                        r--;
+                        r--;  
                     }
                 }
             }
         }
-
-        return result;
+        Set<List<Integer>> set = new HashSet<>(res); 
+        System.out.print(set);
+        List<List<Integer>> ans = new ArrayList<>(set);
+        return ans;
     }
 }
