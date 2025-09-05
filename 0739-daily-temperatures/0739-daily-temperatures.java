aa3696b1e6716574916eb8b69df22be92d426ae8
Nakul Bhar <115773691+Nakkkkkul0130@users.noreply.github.com>
2025-09-05 23:02:12 +0530

    Time: 85 ms (8.08%), Space: 60 MB (75.75%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.java
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -1,21 +1,16 @@
 class Solution {
     public int[] dailyTemperatures(int[] temperatures) {
-        Stack<Integer> st = new Stack<>();
         int n = temperatures.length;
-        int[] result = new int[n];
+        int[] res = new int[n];
+        Stack<Integer> st = new Stack<>();
         for(int i=n-1;i>=0;i--){
-            //pop all indices with lower or equal temp than current index
             while(!st.isEmpty() && temperatures[i]>=temperatures[st.peek()]){
                 st.pop();
             }
-            // if stack has still element,then next warmer day?
-            if(!st.isEmpty()){
-                result[i]=st.peek()-i;
-            }
-            // inserting current index
+            res[i] = st.isEmpty()? 0 : st.peek()-i;
             st.push(i);
-
+            
         }
-        return result;
+        return res;
     }
 }
