6be200f702f822e5c9e4901bf068cf6c65f70bcd
Nakul Bhar <115773691+Nakkkkkul0130@users.noreply.github.com>
2025-09-16 11:34:55 +0530

    Time: 0 ms (100%), Space: 40.1 MB (97.43%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int climbStairs(int n) {
+        // if(n<=3){
+        //     return n;
+        // }
+        // return climbStairs(n-1)+climbStairs(n-2);
+
+        if(n==0 || n==1){
+            return 1;
+        }
+        int prev1 = 1 , prev2=1;
+        for(int i=2;i<=n;i++){
+            int curr = prev1+prev2;
+            prev1=prev2;
+            prev2=curr;
+            
+        }
+        return prev2;
+    }
+}
