986ae765bf33eff4e58dce616cb31f74032a257c
Nakul Bhar <115773691+Nakkkkkul0130@users.noreply.github.com>
2025-09-19 19:48:44 +0530

    Time: 0 ms (100%), Space: 40.8 MB (96.37%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,13 +1,12 @@
 class Solution {
     public int rob(int[] nums) {
-        int first=0;
-        int second=0;
-        for(int num : nums){
-            int currsum = Math.max(second, num+first);
-            first = second;
-            second = currsum;
-        }
-        return second;
-
+       int first = 0;
+       int second = 0;
+       for(int num : nums){
+        int currsum = Math.max(second, first+num);
+        first = second;
+        second = currsum;
+       }
+       return second;
     }
 }
