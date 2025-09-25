4ce0f0adea6a6318c5d5d4facab0e371db1a2416
Nakul Bhar <115773691+Nakkkkkul0130@users.noreply.github.com>
2025-09-25 22:33:33 +0530

    Time: 39 ms (96.69%), Space: 55.2 MB (37.98%) - LeetHub

--- 0493-reverse-pairs/0493-reverse-pairs.java
+++ 0493-reverse-pairs/0493-reverse-pairs.java
@@ -11,28 +11,46 @@ class Solution {
         // return count;
         return mergeSort(nums, 0, nums.length - 1);
     }
-    private int mergeSort(int[] nums, int left, int right) {
-        if (left >= right) return 0;
-        int mid = left + (right - left) / 2;
-        int count = mergeSort(nums, left, mid) + mergeSort(nums, mid + 1, right);
+     private int mergeSort(int[] nums, int left, int right) {
+        if (left >= right) return 0; 
 
+        int mid = left + (right - left) / 2;
+
+        int countLeft = mergeSort(nums, left, mid);
+        int countRight = mergeSort(nums, mid + 1, right);
+
+        int crossCount = countCrossPairs(nums, left, mid, right);
+
+        merge(nums, left, mid, right);
+
+        return countLeft + countRight + crossCount;
+    }
+    private int countCrossPairs(int[] nums, int left, int mid, int right) {
+        int count = 0;
         int j = mid + 1;
+
         for (int i = left; i <= mid; i++) {
-            while (j <= right && (long) nums[i] > 2L * nums[j]) j++;
+            while (j <= right && (long) nums[i] > 2L * nums[j]) {
+                j++;
+            }
             count += j - (mid + 1);
         }
 
+        return count;
+    }
+    private void merge(int[] nums, int left, int mid, int right) {
         int[] temp = new int[right - left + 1];
-        int i = left, k = 0;
-        j = mid + 1;
+        int i = left, j = mid + 1, k = 0;
+
         while (i <= mid && j <= right) {
             if (nums[i] <= nums[j]) temp[k++] = nums[i++];
             else temp[k++] = nums[j++];
         }
+
         while (i <= mid) temp[k++] = nums[i++];
         while (j <= right) temp[k++] = nums[j++];
-        System.arraycopy(temp, 0, nums, left, temp.length);
 
-        return count;
+        System.arraycopy(temp, 0, nums, left, temp.length);
     }
+    
 }
